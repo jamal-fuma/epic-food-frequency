95e224f9c94f818fd552425bce0c4c4713684496
Jamal M. Natour <jamal.natour@fumasoftware.com>
2010-12-07 06:37:47 +0000

    cleanup a bit

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,49 +1,324 @@
-CREATE TABLE baking_fats (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, line INTEGER NOT NULL, food VARCHAR NOT NULL, amount REAL NOT NULL );
-CREATE TABLE cereal_brands (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, code INTEGER UNIQUE NOT NULL, description varchar NOT NULL );
-CREATE TABLE cereal_brands_alternatives (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, cereal_brand_id INTEGER UNIQUE NOT NULL, description varchar NOT NULL );
-CREATE TABLE cereal_types (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, code INTEGER UNIQUE NOT NULL, food VARCHAR NOT NULL, description VARCHAR NOT NULL,weight_id INTEGER NOT NULL );
-CREATE TABLE cereals (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, line INTEGER NOT NULL, amount REAL NOT NULL );
-CREATE TABLE cereals_type_alternatives (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, cereal_type_id INTEGER NOT NULL, description VARCHAR NOT NULL );
-CREATE TABLE food_names (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, code varchar);
-CREATE TABLE food_nutrients (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, food_id integer, nutrient_id integer, quantity float);
-CREATE TABLE foods (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, code VARCHAR NOT NULL, nutrient INTEGER NOT NULL, amount REAL NOT NULL, UNIQUE(code,nutrient) );
-CREATE TABLE frequencies (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, code INTEGER UNIQUE NOT NULL, amount REAL NOT NULL );
-CREATE TABLE frying_fats (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, line INTEGER NOT NULL, amount REAL NOT NULL );
-CREATE TABLE groups (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, code INTEGER UNIQUE NOT NULL, description varchar NOT NULL );
-CREATE TABLE ingredients (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, meal_id INTEGER NOT NULL, food VARCHAR NOT NULL, amount INTEGER NOT NULL, modifier VARCHAR NOT NULL DEFAULT "", UNIQUE(meal_id,food) );
-CREATE TABLE meal_foods (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, meal_id integer, food_id integer, quantity float);
-CREATE TABLE meals (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, line INTEGER UNIQUE NOT NULL, name VARCHAR NOT NULL, description VARCHAR NOT NULL);
-CREATE TABLE migrations (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, version VARCHAR NOT NULL, description VARCHAR NOT NULL );
-CREATE TABLE milk_portions (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, code INTEGER UNIQUE NOT NULL, amount REAL NOT NULL );
-CREATE TABLE milk_type_alternatives (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, milk_type_id INTEGER NOT NULL, description VARCHAR NOT NULL );
-CREATE TABLE milk_types (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, food VARCHAR NOT NULL, description VARCHAR NOT NULL );
-CREATE TABLE modifiers (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, line INTEGER NOT NULL, code VARCHAR NOT NULL, amount REAL NOT NULL );
-CREATE TABLE nutrients (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, code INTEGER UNIQUE NOT NULL, description VARCHAR NOT NULL, units VARCHAR NOT NULL );
-CREATE TABLE question_types (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, description varchar NOT NULL );
-CREATE TABLE questionaires (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, code varchar NOT NULL, filename VARCHAR NOT NULL );
-CREATE TABLE questions (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, question_type_id INTEGER NOT NULL, description varchar NOT NULL );
-CREATE TABLE respondents (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, reference varchar UNIQUE NOT NULL, questionaire_id INTEGER NOT NULL );
-CREATE TABLE responses (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, value varchar NOT NULL, question_id INTEGER NOT NULL, respondent_id INTEGER NOT NULL );
-CREATE TABLE visible_fats (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, meal_id INTEGER NOT NULL, food VARCHAR NOT NULL, amount REAL NOT NULL );
-CREATE TABLE weights (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, code INTEGER UNIQUE NOT NULL, amount REAL NOT NULL );
-CREATE INDEX baking_fats_food_index on baking_fats(food);
-CREATE INDEX foods_code_index on foods(code);
-CREATE UNIQUE INDEX foods_nutrient_index on foods(code,nutrient);
-CREATE INDEX frequencies_line_index on frequencies(code);
-CREATE INDEX frying_fats_line_index on frying_fats(line);
-CREATE INDEX groups_code_index on groups(code);
-CREATE INDEX ingredients_meal_line_index on ingredients(meal_id,food);
-CREATE UNIQUE INDEX meal_foods_index on meal_foods(meal_id,food_id);
-CREATE INDEX meal_line_index on meals(line);
-CREATE INDEX meal_name_index on meals(name);
-CREATE INDEX milk_portion_code_index on milk_portions(code);
-CREATE INDEX milk_types_alt_type_index on milk_type_alternatives(milk_type_id);
-CREATE INDEX milk_types_food_index on milk_types(food);
-CREATE INDEX nutrients_code_index on nutrients(code);
-CREATE INDEX question_types_id_index on question_types(id);
-CREATE INDEX questions_description_index on questions(description);
-CREATE INDEX respondents_reference_and_questionaire_index on respondents(reference,questionaire_id);
-CREATE UNIQUE INDEX respondents_reference_index on respondents(reference);
-CREATE UNIQUE INDEX response_respondent_index on responses(respondent_id,id);
-CREATE INDEX visible_fats_meal_index on visible_fats(meal_id);
-CREATE INDEX weights_code_index on weights(code);
+CREATE TABLE questionaires (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	filename VARCHAR UNIQUE NOT NULL
+);
+
+CREATE TABLE people (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	reference VARCHAR UNIQUE NOT NULL
+);
+
+CREATE TABLE meals (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	name VARCHAR UNIQUE NOT NULL,
+	description VARCHAR NOT NULL
+);
+CREATE UNIQUE INDEX index_meals_on_name ON meals(name);
+
+CREATE TABLE nutrients (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	code INTEGER UNIQUE NOT NULL,
+	description VARCHAR NOT NULL,
+	units VARCHAR NOT NULL
+);
+CREATE UNIQUE INDEX index_nutrients_on_code ON nutrients(code);
+
+CREATE TABLE weights (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        amount REAL NOT NULL
+);
+
+CREATE TABLE frequencies (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        amount REAL NOT NULL
+);
+
+CREATE TABLE portions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        amount FLOAT NOT NULL
+);
+
+
+CREATE TABLE foods (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	name VARCHAR UNIQUE NOT NULL,
+	description VARCHAR NOT NULL default ""
+);
+CREATE UNIQUE INDEX index_foods_on_name ON foods(name);
+
+
+CREATE TABLE questionaire_people (
+	questionaire_id INTEGER NOT NULL,
+	person_id INTEGER NOT NULL,
+        UNIQUE(questionaire_id,person_id)
+);
+
+CREATE INDEX index_questionaire_people_on_questionaire ON questionaire_people(questionaire_id);
+
+CREATE TABLE food_nutrients (
+	food_id INTEGER NOT NULL,
+	nutrient_id INTEGER NOT NULL,
+	amount FLOAT NOT NULL default 0,
+        UNIQUE(food_id,nutrient_id)
+);
+CREATE INDEX index_food_nutrients_on_food ON food_nutrients(food_id);
+
+CREATE TABLE meal_foods (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	meal_id INTEGER NOT NULL,
+	food_id INTEGER default NULL,
+	amount  FLOAT   NOT NULL,
+        modifier INTEGER NOT NULL default 0
+);
+CREATE INDEX index_meal_foods_on_meal ON meal_foods(meal_id);
+CREATE INDEX index_meal_foods_on_meal_and_modifier ON meal_foods(meal_id,modifier);
+
+CREATE TABLE cereals (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	food_id     INTEGER UNIQUE NOT NULL,
+        weight_id   INTEGER NOT NULL
+);
+
+CREATE TABLE milks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	code INTEGER UNIQUE NOT NULL,
+	food_id     INTEGER NOT NULL
+);
+
+CREATE TABLE person_fats (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	person_id   INTEGER NOT NULL,
+	food_id     INTEGER NOT NULL,
+        modifier INTEGER NOT NULL default 0
+);
+CREATE INDEX index_person_fats_on_person_and_modifier_and_food_id ON person_fats(person_id,food_id,modifier);
+CREATE INDEX index_person_fats_on_person_and_modifier ON person_fats(person_id,modifier);
+
+CREATE TABLE person_weights (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	person_id   INTEGER NOT NULL,
+	weight_id   INTEGER NOT NULL,
+        modifier INTEGER NOT NULL default 0
+);
+CREATE INDEX index_person_weights_on_person_and_modifier_and_weight ON person_weights(person_id,weight_id,modifier);
+
+CREATE TABLE person_cereals (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	person_id   INTEGER NOT NULL,
+	food_id     INTEGER NOT NULL,
+	weight_id   INTEGER NOT NULL,
+        modifier    INTEGER NOT NULL default 3
+);
+CREATE INDEX index_person_cereals_on_person_and_modifier ON person_cereals(person_id,modifier);
+CREATE INDEX index_person_cereals_on_person_food_and_modifier_and_weight ON person_cereals(person_id,food_id,weight_id,modifier);
+
+CREATE TABLE person_meals (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	person_id       INTEGER NOT NULL,
+	meal_id         INTEGER NOT NULL,
+        frequency_id    INTEGER NOT NULL
+);
+
+CREATE INDEX index_person_meals_on_person_meal_and_frequency ON person_meals(person_id,meal_id,frequency_id);
+CREATE INDEX index_person_meals_on_meal_and_frequency ON person_meals(meal_id,frequency_id);
+
+CREATE TABLE person_foods (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	person_id INTEGER NOT NULL,
+	food_id INTEGER NOT NULL,
+        frequency_id    INTEGER NOT NULL,	
+        portion_id INTEGER NOT NULL
+);
+CREATE INDEX index_person_foods_on_person ON person_foods(person_id);
+CREATE INDEX index_person_foods_on_frequency_id_and_portion_id ON person_foods(frequency_id,portion_id);
+
+
+CREATE VIEW person_fat_totals_vw AS SELECT 
+    person_id,
+    count(person_id) AS entries, 
+    modifier 
+FROM 
+    person_fats 
+GROUP BY 
+    person_id,
+    modifier ;
+
+CREATE VIEW person_cereal_totals_vw AS SELECT 
+    person_id,
+    count(person_id) AS entries, 
+    modifier 
+FROM 
+    person_cereals 
+GROUP BY 
+    person_id,
+    modifier ;
+
+CREATE VIEW person_milk_totals_vw AS SELECT 
+    person_id,
+    count(person_id) AS entries
+FROM 
+    person_foods 
+GROUP BY 
+    person_id;
+
+
+CREATE VIEW person_fat_foods_vw AS SELECT 
+    person_meals.person_id, 
+    person_meals.meal_id,
+    person_fats.food_id, 
+    ( (frequencies.amount) * (meal_foods.amount / person_fat_totals_vw.entries)) AS amount
+FROM 
+    person_meals,
+    meal_foods,
+    person_fats,
+    person_fat_totals_vw,
+    frequencies
+WHERE 
+    person_fats.person_id               = person_fat_totals_vw.person_id
+    AND person_fats.modifier            = person_fat_totals_vw.modifier
+    AND person_meals.person_id          = person_fats.person_id
+    AND person_meals.meal_id            = meal_foods.meal_id
+    AND person_meals.frequency_id       = frequencies.id
+    AND person_fats.modifier            = meal_foods.modifier
+ORDER BY
+    person_fats.person_id;
+
+
+CREATE VIEW person_cereal_foods AS SELECT 
+    person_cereals.person_id, 
+    meal_foods.meal_id,
+    person_cereals.food_id, 
+    ( (frequencies.amount) * ((meal_foods.amount / person_cereal_totals_vw.entries) / weights.amount) ) AS amount
+FROM 
+    person_meals,
+    person_cereals,
+    person_cereal_totals_vw,
+    frequencies,
+    weights,
+    meal_foods
+WHERE 
+    person_cereals.person_id             = person_meals.person_id 
+    AND person_cereals.modifier          = meal_foods.modifier
+    AND person_cereals.weight_id         = weights.id
+    AND person_meals.frequency_id        = frequencies.id
+    AND meal_foods.meal_id               = person_meals.meal_id
+    AND person_cereals.person_id         = person_cereal_totals_vw.person_id 
+    AND person_cereal_totals_vw.modifier = person_cereals.modifier
+ORDER BY
+    person_meals.person_id;
+
+
+CREATE VIEW person_meat_foods_vw AS SELECT 
+    person_weights.person_id, 
+    meal_foods.meal_id,
+    meal_foods.food_id, 
+    ((meal_foods.amount * weights.amount) * frequencies.amount) AS amount
+FROM 
+    person_weights,
+    person_meals,
+    frequencies,
+    weights,
+    meal_foods
+WHERE 
+    person_weights.person_id        = person_meals.person_id 
+    AND person_weights.weight_id    = weights.id
+    AND meal_foods.modifier         = person_weights.modifier
+    AND person_meals.frequency_id   = frequencies.id
+    AND meal_foods.meal_id          = person_meals.meal_id
+ORDER BY
+    person_weights.person_id;
+
+/* dirty hack to allow combining with meal reporting  */
+CREATE VIEW person_milk_foods_vw AS SELECT 
+    person_foods.person_id, 
+    0 AS meal_id,
+    person_foods.food_id, 
+    ((portions.amount * frequencies.amount) / person_milk_totals_vw.entries) AS amount
+FROM
+    person_foods,
+    frequencies,
+    portions,
+    person_milk_totals_vw
+WHERE 
+    person_foods.frequency_id   = frequencies.id
+    AND person_foods.portion_id = portions.id
+    AND person_foods.person_id  = person_milk_totals_vw.person_id
+ORDER BY
+    person_foods.person_id;
+
+CREATE VIEW person_simple_foods_vw AS SELECT 
+    person_meals.person_id, 
+    meal_foods.meal_id,
+    meal_foods.food_id, 
+    (meal_foods.amount * frequencies.amount) AS amount
+FROM
+    person_meals,
+    frequencies,
+    meal_foods
+WHERE 
+    meal_foods.modifier = 0 
+    AND person_meals.frequency_id = frequencies.id
+    AND meal_foods.meal_id = person_meals.meal_id
+ORDER BY
+    person_meals.person_id;
+
+CREATE VIEW person_food_vw AS SELECT 
+    person_id,meal_id,food_id,amount FROM person_fat_foods_vw
+WHERE
+    amount > 0
+UNION ALL SELECT 
+    person_id,meal_id,food_id,amount FROM person_cereal_foods
+WHERE
+    amount > 0
+UNION ALL SELECT 
+    person_id,meal_id,food_id,amount FROM person_meat_foods_vw
+WHERE
+    amount > 0
+UNION ALL SELECT 
+    person_id,meal_id,food_id,amount FROM person_simple_foods_vw
+WHERE
+    amount > 0
+UNION ALL SELECT 
+    person_id,meal_id,food_id,amount FROM person_milk_foods_vw
+WHERE
+    amount > 0
+ORDER BY person_id,meal_id,food_id;
+
+CREATE TABLE meal_nutrients_tmp (
+	meal_id INTEGER NOT NULL,
+	nutrient_code INTEGER NOT NULL,
+	quantity  FLOAT   NOT NULL
+);
+CREATE INDEX index_meal_nutrients_tmp_on_meal_and_nutrient ON meal_nutrients_tmp(meal_id,nutrient_code);
+
+CREATE VIEW meal_nutrients_vw AS SELECT
+    meal_id,nutrient_code,sum(quantity) as quantity
+FROM
+    meal_nutrients_tmp
+GROUP BY
+    meal_id,nutrient_code;
+
+CREATE VIEW nutrients_vw AS SELECT
+    nutrient_code, sum(quantity) as quantity
+FROM
+    meal_nutrients_vw
+GROUP BY
+    nutrient_code;
+
+
+CREATE TRIGGER trigger_questionaire_clean AFTER DELETE ON questionaires
+BEGIN 
+    DELETE FROM questionaire_people WHERE questionaire_id = OLD.id; 
+END;
+
+CREATE TRIGGER trigger_questionaire_people_clean AFTER DELETE ON questionaire_people
+BEGIN
+    DELETE FROM people              WHERE id        = OLD.person_id;
+    DELETE FROM questionaire_people WHERE person_id = OLD.person_id;
+    DELETE FROM person_meals        WHERE person_id = OLD.person_id;
+    DELETE FROM person_fats         WHERE person_id = OLD.person_id;
+    DELETE FROM person_foods        WHERE person_id = OLD.person_id;
+    DELETE FROM person_cereals      WHERE person_id = OLD.person_id;
+    DELETE FROM person_weights      WHERE person_id = OLD.person_id;
+    DELETE FROM meal_nutrients_tmp ;
+END;
--- test/test_db.cpp
+++ test/test_db.cpp
@@ -13,26 +13,10 @@
 #include "dao/Milk.hpp"
 #include "import/Import.hpp"
 
-bool load_foods(const std::string & filename);
-bool load_nutrients(const std::string & filename);
-bool load_meals(const std::string & filename);
-bool load_food_nutrients(const std::string & filename);
-bool load_meal_foods(const std::string & filename);
-bool load_weights(const std::string & filename);
-bool load_frequencies(const std::string & filename);
-bool load_portions(const std::string & filename);
-bool load_cereals(const std::string & filename);
-bool load_milks(const std::string & filename);
-
-void test_imports();
-
-
 int
 main(int argc, char **argv)
 {
     std::string conf    = "/home/me/workspace/clone/epic-food-frequency-0.0.1/build/client.conf";
-    std::string schema  = "/home/me/workspace/clone/epic-food-frequency-0.0.1/sql/model.sql" ;
-    std::string dbase   = "/home/me/workspace/clone/epic-food-frequency-0.0.1/build/test/foods.db" ;
     
     Epic::Logging::open("test.log");
 
@@ -44,17 +28,7 @@ main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    // overwrite the schema variable listed in config file with the new version
-    if(!Epic::Config::insert("schema",schema,true))
-        return EXIT_FAILURE;
-
-    // overwrite the database variable listed in config file with the new version
-    if(!Epic::Config::insert("database",dbase,true))
-        return EXIT_FAILURE;
-
     Epic::Database::connect();
-
-    test_imports();
 
     // find all meals with visible fat
     std::vector<Epic::DAO::Meal> meals; 
@@ -98,108 +72,3 @@ main(int argc, char **argv)
 
     return EXIT_SUCCESS;
 }
-
-void test_imports()
-{
-    std::string foods_file  = "/home/me/workspace/clone/epic-food-frequency-0.0.1/import/model/foods.csv" ;
-    std::string food_nutrients_file  = "/home/me/workspace/clone/epic-food-frequency-0.0.1/import/model/food_nutrients.csv" ;
-    std::string meal_foods_file  = "/home/me/workspace/clone/epic-food-frequency-0.0.1/import/model/meal_foods.csv" ;
-    std::string portions_file  = "/home/me/workspace/clone/epic-food-frequency-0.0.1/import/model/portions.csv" ;
-    std::string cereals_file  = "/home/me/workspace/clone/epic-food-frequency-0.0.1/import/model/cereals.csv" ;
-    std::string milks_file  = "/home/me/workspace/clone/epic-food-frequency-0.0.1/import/model/milks.csv" ;
-
-    std::string s;
-    load_foods(foods_file);
-    if( Epic::Config::find("nutrients",s) )
-    {
-        load_nutrients(s);
-    }
-    else
-    {
-        std::cerr << "nutrients not present in config" << std::endl;
-    }
-
-    load_food_nutrients(food_nutrients_file);
-
-    if( Epic::Config::find("meals",s) )
-    {
-        load_meals(s);
-    }
-    else
-    {
-        std::cerr << "meals not present in config" << std::endl;
-    }
-    if( Epic::Config::find("weights",s) )
-    {
-        load_weights(s);
-    }
-    else
-    {
-        std::cerr << "weights not present in config" << std::endl;
-    }
-    if( Epic::Config::find("frequencies",s) )
-    {
-        load_frequencies(s);
-    }
-    else
-    {
-        std::cerr << "frequencies not present in config" << std::endl;
-    }
-
-    load_meal_foods(meal_foods_file);
-    load_portions(portions_file);
-    load_cereals(cereals_file);
-    load_milks(milks_file);
-}
-
-
-
-bool load_foods(const std::string & filename)
-{
-    return Epic::DAO::Food::load(filename);
-}
-
-bool load_meals(const std::string & filename)
-{
-    return Epic::DAO::Meal::load(filename);
-}
-
-bool load_nutrients(const std::string & filename)
-{
-    return Epic::DAO::Nutrient::load(filename);
-}
-
-bool load_food_nutrients(const std::string & filename)
-{
-    return Epic::DAO::FoodNutrient::load(filename);
-}
-
-bool load_meal_foods(const std::string & filename)
-{
-    return Epic::DAO::MealFood::load(filename);
-}
-
-bool load_weights(const std::string & filename)
-{
-    return Epic::DAO::Weight::load(filename);
-}
-
-bool load_portions(const std::string & filename)
-{
-    return Epic::DAO::Portion::load(filename);
-}
-
-bool load_frequencies(const std::string & filename)
-{
-    return Epic::DAO::Frequency::load(filename);
-}
-
-bool load_cereals(const std::string & filename)
-{
-    return Epic::DAO::Cereal::load(filename);
-}
-bool load_milks(const std::string & filename)
-{
-    return Epic::DAO::Milk::load(filename);
-}
-
