5344969a4ef7aba3d7c4aa2a25b23a4325e625c0
Jamal M. Natour <jamal.natour@fumasoftware.com>
2011-04-30 03:14:00 +0100

    compile cleanly

--- src/application.cpp
+++ src/application.cpp
@@ -16,6 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "client/Application.hpp"
+#include "client/ReportWriter.hpp"
 
 int
 Epic::Client::Application::run()
