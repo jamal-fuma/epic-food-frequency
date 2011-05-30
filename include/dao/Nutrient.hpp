0132b1b52ab63daf87d89d015f725a979007f5cf
Jamal M. Natour <jamal.natour@fumasoftware.com>
2011-05-30 23:03:49 +0100

    seperated libdao builds

--- include/dao/Nutrient.hpp
+++ include/dao/Nutrient.hpp
@@ -18,7 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef EPIC_DAO_NUTRIENT_HPP
 #define EPIC_DAO_NUTRIENT_HPP
 
-#include "Epic_lib.hpp"
 #include <vector>
 
 namespace Epic
