fa8395924e68288f17e9811e31a582a5b71bce82
Jamal M. Natour <jamal.natour@fumasoftware.com>
2011-06-16 21:18:35 +0100

    pulled in devel's view of build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
-export ac_cv_func_realloc_0_nonnull=yes
-export ac_cv_func_malloc_0_nonnull=yes
-
-export LD_FLAGS="-lwinsock32 -lws2_32 -lnetapi32 -luserenv -lshlwapi -lmingw32"
-export CFLAGS="--static"
-../epic-food-frequency-0.0.2-a/configure --prefix=/home/me/workspace/clone/windows --host=i586-mingw32 --enable-maintainer-mode
+#!/bin/bash
+export CXX="ccache g++"
+export CC="ccache gcc"
+PREFIX=`pwd`/root
+( cd ${PREFIX}/../;  ../autogen.sh --prefix=${PREFIX}; )
 make
-i586-mingw32-g++ epic_ffq-client.o src/.libs/libEpic.a -static -o epic_ffq.exe
+make distcheck
+exit $?
