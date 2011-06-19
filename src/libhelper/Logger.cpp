cc41107aa19fd7afa5536f5958d33148310e948d
Jamal M. Natour <jamal.natour@fumasoftware.com>
2011-06-19 14:54:04 +0100

    Moved Logger class into libhelper

--- src/libhelper/Logger.cpp
+++ src/libhelper/Logger.cpp
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "logging/Logger.hpp"
+#include "Logger.hpp"
 
 void Epic::Logging::error(const std::string & str)
 {
