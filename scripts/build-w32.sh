9ec40a0d3971ecfbd6aa0e858e024e7431009ce6
Jamal M. Natour <jamal.natour@fumasoftware.com>
2011-07-25 00:34:06 +0100

    updated windows build script

--- scripts/build-w32.sh
+++ scripts/build-w32.sh
@@ -1,55 +1,46 @@
+#!/bin/bash
+
 # build for relatively old machines
-HOST=${HOST:-i586-mingw32}
+TARGET_ARCH=${TARGET_ARCH:-i586-mingw32}
 
 # use ccache to speed up compilation if in path
-CC="ccache ${HOST}-gcc";
-CXX="ccache ${HOST}-g++";
-
-# strip ccache from the commands if they are not present
-which cche 2>  /dev/null;
-if [ $? -eq 0 ] ;
-then
-    export CC="${CC/ccache /}";
-    export CXX="${CXX/ccache /}";
-fi
-
-# default prefix is under the c drive
-PREFIX=${PREFIX:-"c:/epic"}
-
-# windows executables end with a suffix
-EXT=${EXT:-.exe}
-
-# Windows builds need these set otherwise the cross-configure fails
-export ac_cv_func_realloc_0_nonnull=yes
-export ac_cv_func_malloc_0_nonnull=yes
+export CC="ccache ${TARGET_ARCH}-gcc";
+export CXX="ccache ${TARGET_ARCH}-g++";
 
 # Windows builds link against win32
-export LD_FLAGS="-lwinsock32 -lws2_32 -lnetapi32 -luserenv -lshlwapi -lmingw32"
+#export LDFLAGS="-lwinsock32 -lws2_32 -lnetapi32 -luserenv -lshlwapi -lmingw32 -static -static-libgcc"
+export LDFLAGS="-static -static-libgcc"
 
 # cross-compilation builds link statically
-export CFLAGS="--static"
+export CFLAGS="-O2 -g -Wall -mwindows"
+
+# windows executables end with a suffix
+export EXT=${EXT:-.exe}
 
 # extract version of build
 project_with_version=`grep AC_INIT configure.ac | cut -f 1,2 -d',' | tr -d '[[]]' | sed -e '/^[^(]\+./{ s///; s/,/-/; }'`
 project=`grep AC_INIT configure.ac | cut -f 1 -d',' | tr -d '[[]]' | sed -e '/^[^(]\+./{ s///; s/,/-/; }'`
+
+# default prefix is under the c drive
+export PREFIX=${PREFIX:-"c:/${project_with_version}"}
 
 # serialise extracted version of build
 echo "${project_with_version}" > version.txt
 echo "Building ${project_with_version}"
 
 # cross-configure
-mkdir -p build/dest;
-cd build;
-../autogen.sh --prefix=${PREFIX} --host=${HOST} --enable-maintainer-mode
+./autogen.sh;
 
-# cross-compile
-make;
+mkdir -p build-w32/dest;
+cd build-w32;
 
-# install to staging
-make DESTDIR=`pwd`/dest install;
+# Windows builds need these set otherwise the cross-configure fails
+export ac_cv_func_realloc_0_nonnull=yes
+export ac_cv_func_malloc_0_nonnull=yes
 
-# package
-tar -cvjf epic-win32.tbz `pwd`/dest
-
-
-# ${CXX} epic_ffq-client.o src/libhelper/.libs/libEpicHelper.a src/libcsv/.libs/libEpicCSV.a src/libsqlite/.libs/libSqlite.a src/libdao/.libs/libEpicDAO.a -static -o epic_ffq${EXT}
+# cross-compile and install to staging for packaging
+../configure --prefix=${PREFIX} --host=${TARGET_ARCH}  --enable-maintainer-mode --enable-wine-tests && \
+        make && \
+        make check && \
+        make DESTDIR="`pwd`/dest/" install && \
+        tar -cvjf ${project_with_version}_win32.tbz `pwd`/dest ;
