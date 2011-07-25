100114093a8f5ebba6f243f86e3d049f97d8b6f6
Jamal M. Natour <jamal.natour@fumasoftware.com>
2011-07-25 22:22:01 +0100

    updated darwin build script

--- scripts/build-darwin.sh
+++ scripts/build-darwin.sh
@@ -19,4 +19,19 @@
 
 mkdir -p build-darwin/dest;
 
-cd build-darwin/; ( export CC="ccache /opt/i686-apple-darwin10/toolchain/bin/i686-apple-darwin10-gcc"; export CXX="ccache /opt/i686-apple-darwin10/toolchain/bin/i686-apple-darwin10-g++";  ../configure --enable-maintainer-mode  --host=i686-apple-darwin10 --prefix="/Applications/epic-food-frequency.app"; make  );
+cd build-darwin/;
+
+export CC="ccache /opt/i686-apple-darwin10/toolchain/bin/i686-apple-darwin10-gcc";
+export CXX="ccache /opt/i686-apple-darwin10/toolchain/bin/i686-apple-darwin10-g++";
+
+# extract version of build
+project_with_version=`grep AC_INIT configure.ac | cut -f 1,2 -d',' | tr -d '[[]]' | sed -e '/^[^(]\+./{ s///; s/,/-/; }'`
+
+project=`grep AC_INIT configure.ac | cut -f 1 -d',' | tr -d '[[]]' | sed -e '/^[^(]\+./{ s///; s/,/-/; }'`
+
+../configure    --enable-maintainer-mode   \
+                --host=i686-apple-darwin10 \
+                --prefix="/Applications/epic-food-frequency.app" && \
+        make && \
+        make DESTDIR="`pwd`/dest/" install && \
+        tar -cvjf ${project_with_version}_osx.tbz `pwd`/dest;
